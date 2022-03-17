f0ee206df39cab5bb54b6aec6e12c7760aa2c2b4
Vladislav Prekel <misterptits@yandex.ru>
2022-03-17 20:44:22 +0700

    gadt

--- common/scene.ml
+++ common/scene.ml
@@ -47,15 +47,89 @@ end
 
 module Figure2 = struct
   module Formula = struct
-    module CoefficientVar = struct
-      type t = A
+    module CoefValue = struct
+      type _ t =
+        | Scalar : float -> float t
+        | Vector : (float * float) -> (float * float) t
     end
 
-    type t = (int, float * CoefficientVar.t, Int.comparator_witness) Map.t
+    module CoefficientVar = struct
+      type ('underlying, 'result) t =
+        | Scalar : (float, float) t
+        | Vector : (float * float, float * float) t
+        | VecOfScalars : (('a, float) t * ('b, float) t) -> ('a * 'b, float * float) t
+        | Sum : ('a, float) t * ('b, float) t -> ('a * 'b, float) t
+        | Mult : ('a, float) t * ('b, float) t -> ('a * 'b, float) t
+        | VecX : ('a, float * float) t -> ('a, float) t
+        | VecY : ('a, float * float) t -> ('a, float) t
+        | VecLen : ('a, float * float) t -> ('a, float) t
+        | VecNormalized : ('a, float * float) t -> ('a, float * float) t
+        | VecMultScalar :
+            ('a, float * float) t * ('b, float) t
+            -> ('a * 'b, float * float) t
+        | VecInverted : ('a, float * float) t -> ('a, float * float) t
+
+      let _ = Scalar
+
+      let rec calc
+          : type underlying result. underlying -> (underlying, result) t -> result
+        =
+       fun u -> function
+        | Scalar -> u
+        | Vector -> u
+        | VecOfScalars (l, r) ->
+          let ul, ur = u in
+          calc ul l, calc ur r
+        | Sum (l, r) ->
+          let ul, ur = u in
+          Float.(calc ul l + calc ur r)
+        | Mult (l, r) ->
+          let ul, ur = u in
+          Float.(calc ul l * calc ur r)
+        | VecX e ->
+          let x, _y = calc u e in
+          x
+        | VecY e ->
+          let _, y = calc u e in
+          y
+        | VecLen e ->
+          let x, y = calc u e in
+          Float.(sqrt ((x ** 2.) + (y ** 2.)))
+        | VecNormalized e ->
+          let x, y = calc u e in
+          let len = Float.(sqrt ((x ** 2.) + (y ** 2.))) in
+          Float.(x / len, y / len)
+        | VecMultScalar (a, b) ->
+          let ul, ur = u in
+          let x, y = calc ul a in
+          let z = calc ur b in
+          Float.(x * z, y * z)
+        | VecInverted e ->
+          let x, y = calc u e in
+          Float.(-x, -y)
+     ;;
+
+      let c () = calc (1., 3.) Vector
+      let c () = calc 2. Scalar
+      let c () = calc (1., 3.) (VecX Vector)
+      let v0_vec = Vector
+      let v0_x = VecX v0_vec
+      let v0_y = VecY v0_vec
+      let sum () = calc ((2., 3.), 1.) (VecMultScalar (v0_vec, Scalar))
+    end
+
+    module Var = struct
+      type t =
+        | Scalar of float CoefficientVar.t
+        | Vector of (float * float) CoefficientVar.t
+    end
+
+    type t = (int, float * float CoefficientVar.t, Int.comparator_witness) Map.t
   end
 
   type t =
     { id : int
+    ; vars : (string, float Formula.CoefficientVar.t, String.comparator_witness) Map.t
     ; x : Formula.t
     }
 end
