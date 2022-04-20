918f55473142afdfdf070ba714716dd290dacdcd
Vladislav Prekel <misterptits@yandex.ru>
2022-04-20 15:25:49 +0700

    server

--- server/main.ml
+++ server/main.ml
@@ -1,89 +1,103 @@
-let home =
-  {|<html>
-  <body>
-    <form>
-      <input type="submit" value="Send">
-      <input type="text" id="message" size="64" autofocus>
-    </form>
-    <script>
-      let message = document.getElementById("message");
-      let chat = document.querySelector("body");
-      let socket = new WebSocket("ws://" + window.location.host + "/websocket");
+open Core
+open Lwt.Let_syntax
+module SC = Chapgame.Scene.Make (Float) ((val Chapgame.Utils.make_consts ~eps:1e-6))
 
-      socket.onmessage = function (event) {
-        let item = document.createElement("div");
-        item.innerText = event.data;
-        chat.appendChild(item);
-      };
+module Client = struct
+  module Id = Chapgame.Utils.MakeIntId (struct
+    let module_name = "Client.Id"
+  end)
 
-      document.querySelector("form").onsubmit = function () {
-        if (socket.readyState != WebSocket.OPEN)
-          return false;
-        if (!message.value)
-          return false;
+  type t = Dream.websocket
+end
 
-        socket.send(message.value);
-        message.value = "";
-        return false;
-      };
-    </script>
-  </body>
-  </html>|}
+module Clients = struct
+  type t = (Client.Id.t, Client.t) Hashtbl.t
+end
+
+module Room = struct
+  module Id = Chapgame.Utils.MakeIntId (struct
+    let module_name = "Room.Id"
+  end)
+
+  type t =
+    { model : SC.Model.t
+    ; clients : Clients.t
+    }
+
+  let init () =
+    { model = SC.Model.init ~g:10.; clients = Hashtbl.create (module Client.Id) }
+  ;;
+end
+
+module Rooms = struct
+  type t = (Room.Id.t, Room.t) Hashtbl.t
+
+  let init () = Hashtbl.create (module Room.Id)
+end
+
+let rooms_field : Rooms.t Dream.field = Dream.new_field ()
+let rooms_var = Rooms.init ()
+
+let rooms_middleware inner_handler request =
+  Dream.set_field request rooms_field rooms_var;
+  inner_handler request
 ;;
 
-type message = unit
+module DreamExt = struct
+  let sexp ?status ?code ?headers ?mach sexp =
+    let body =
+      match mach with
+      | Some true -> Sexp.to_string_mach sexp
+      | _ -> Sexp.to_string_hum sexp
+    in
+    let response = Dream.response ?status ?code ?headers body in
+    Dream.set_header response "Content-Type" "text/plain";
+    Lwt.return response
+  ;;
+end
 
-let clients : (int, Dream.websocket) Hashtbl.t = Hashtbl.create 5
-
-let track =
-  let last_client_id = ref 0 in
-  fun websocket ->
-    last_client_id := !last_client_id + 1;
-    Hashtbl.replace clients !last_client_id websocket;
-    !last_client_id
-;;
-
-let forget client_id = Hashtbl.remove clients client_id
-
-let send message =
-  Hashtbl.to_seq_values clients
-  |> List.of_seq
-  |> Lwt_list.iter_p (fun client -> Dream.send client message)
-;;
-
-let handle_client client =
-  let client_id = track client in
-  let rec loop () =
-    match%lwt Dream.receive client with
-    | Some message ->
-      let%lwt () = send message in
-      loop ()
-    | None ->
-      forget client_id;
-      Dream.close_websocket client
-  in
-  loop ()
+let room_of_request request =
+  let rooms = Dream.field request rooms_field |> Option.value_exn in
+  let room_id = Dream.param request "room_id" |> Room.Id.of_string in
+  rooms, Hashtbl.find_exn rooms room_id, room_id
 ;;
 
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router
-       [ Dream.get "/" (fun _ -> Dream.html home)
-       ; Dream.get "/websocket" (fun _ -> Dream.websocket handle_client)
-       ; Dream.get "/room/:room_id/ws" (fun request ->
-             let _room_id = Dream.param request "room_id" in
-             Dream.websocket (fun client ->
-                 let client_id = track client in
-                 let rec loop () =
-                   match%lwt Dream.receive client with
-                   | Some message ->
-                     let%lwt () = send message in
-                     loop ()
-                   | None ->
-                     forget client_id;
-                     Dream.close_websocket client
+       [ Dream.get "/" (fun _ -> Dream.html "")
+       ; Dream.scope
+           "/room"
+           [ rooms_middleware ]
+           [ Dream.post "/create" (fun request ->
+                 let rooms = Dream.field request rooms_field |> Option.value_exn in
+                 let id = Room.Id.next () in
+                 Hashtbl.add_exn rooms ~key:id ~data:(Room.init ());
+                 DreamExt.sexp [%sexp (id : Room.Id.t)])
+           ; Dream.get "/:room_id" (fun request ->
+                 let _, room, _ = room_of_request request in
+                 DreamExt.sexp [%sexp (room.model : SC.Model.t)])
+           ; Dream.post "/:room_id/action" (fun request ->
+                 let rooms, room, room_id = room_of_request request in
+                 let%bind body = Dream.body request in
+                 let action = body |> Sexp.of_string |> [%of_sexp: SC.Action.t] in
+                 let model, diff =
+                   SC.Engine.recv_with_diff room.model ~action:(`Action action)
                  in
-                 loop ()))
+                 Hashtbl.update rooms room_id ~f:(function
+                     | Some prev -> { prev with model }
+                     | _ -> assert false);
+                 DreamExt.sexp [%sexp (diff : SC.Model.Diff.diff)])
+           ; Dream.get "/:room_id/ws" (fun request ->
+                 let _room_id = Dream.param request "room_id" in
+                 Dream.websocket (fun client ->
+                     let rec loop () =
+                       match%bind Dream.receive client with
+                       | Some _message -> loop ()
+                       | None -> Dream.close_websocket client
+                     in
+                     loop ()))
+           ]
        ]
 ;;
