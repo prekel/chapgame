b825d698f70d02267fdde7f58a5c3ce04034996a
Vladislav Prekel <misterptits@yandex.ru>
2022-05-03 19:38:42 +0700

    stream

--- client/websocket.ml
+++ client/websocket.ml
@@ -9,7 +9,7 @@ type t =
 
 let equal = phys_equal
 
-let connect url ~on_message =
+let connect url ~on_message ~on_close =
   let url = Uri.to_string url in
   let websocket = new%js WebSockets.webSocket (Js.string url) in
   websocket##.onmessage
@@ -17,8 +17,24 @@ let connect url ~on_message =
            let data = Js.to_string event##.data in
            on_message data;
            Js._false);
+  websocket##.onclose
+    := Dom.handler (fun (event : _ WebSockets.closeEvent Js.t) ->
+           let data = event##.code in
+           on_close data;
+           Js._false);
   { websocket; url }
 ;;
 
 let send { websocket; _ } data = websocket##send (Js.string data)
 let close { websocket; _ } = websocket##close
+
+let stream url =
+  let stream, push = Lwt_stream.create () in
+  let t =
+    connect
+      url
+      ~on_message:(fun message -> push (Some message))
+      ~on_close:(fun _ -> push None)
+  in
+  stream, t
+;;
--- client/websocket.mli
+++ client/websocket.mli
@@ -1,5 +1,6 @@
 type t [@@deriving sexp, equal]
 
-val connect : Uri.t -> on_message:(string -> unit) -> t
+val connect : Uri.t -> on_message:(string -> unit) -> on_close:(int -> unit) -> t
 val send : t -> string -> unit
 val close : t -> unit
+val stream : Uri.t -> string Lwt_stream.t * t
