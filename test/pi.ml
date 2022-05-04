fdcd9eff59a429415c01a9648805df1d15e99567
Vladislav Prekel <misterptits@yandex.ru>
2022-05-04 07:37:23 +0700

    test ci

--- test/pi.ml
+++ test/pi.ml
@@ -8,35 +8,27 @@ let model_pi m2 =
   |> S.Engine.recv
        ~action:
          { time = 0.
-         ; action = AddBody { id = id1; x0 = 5.; y0 = 2.; r = 1.; mu = 0.; m = 1. }
+         ; action = AddBody { id = id1; x0 = 350.; y0 = 200.; r = 100.; mu = 0.; m = 1. }
          ; timeout = Some 0.
          }
   |> S.Engine.recv
        ~action:
          { time = 0.
-         ; action =
-             AddBody
-               { id = id2
-               ; x0 = 10.
-               ; y0 = 2.
-               ; r = 1.
-               ; mu = 0.
-               ; m = 10. ** Int.to_float m2
-               }
+         ; action = AddBody { id = id2; x0 = 700.; y0 = 200.; r = 100.; mu = 0.; m = m2 }
          ; timeout = Some 0.
          }
   |> S.Engine.recv
-       ~action:{ time = 0.; action = AddPoint { x = 0.; y = 2. }; timeout = Some 0. }
+       ~action:{ time = 0.; action = AddPoint { x = 0.; y = 200. }; timeout = Some 0. }
   |> S.Engine.recv
        ~action:
          { time = 0.
-         ; action = GiveVelocity { id = id1; v0 = -1., 0. }
+         ; action = GiveVelocity { id = id1; v0 = -100., 0. }
          ; timeout = Some 0.
          }
   |> S.Engine.recv
        ~action:
          { time = 0.
-         ; action = GiveVelocity { id = id2; v0 = -1., 0. }
+         ; action = GiveVelocity { id = id2; v0 = -100., 0. }
          ; timeout = Some 0.
          }
   |> S.Engine.recv ~action:{ time = 0.; action = Empty; timeout = None }
@@ -45,20 +37,21 @@ let model_pi m2 =
 let model_to_pi S.Model.{ scenes; _ } = (S.Model.Scenes.to_map scenes |> Map.length) - 1
 
 let%expect_test "3.1" =
-  let model = model_pi 2 in
+  let model = model_pi 1e2 in
   (* print_s [%sexp (model : S.Model.t)]; *)
   print_s [%sexp (model_to_pi model : int)];
-  [%expect
-    {|
+  [%expect {|
     31 |}]
 ;;
 
 let%expect_test "pi" =
-  print_s [%sexp (2 |> model_pi |> model_to_pi : int)];
-  print_s [%sexp (4 |> model_pi |> model_to_pi : int)];
-  print_s [%sexp (6 |> model_pi |> model_to_pi : int)];
-  print_s [%sexp (8 |> model_pi |> model_to_pi : int)];
+  print_s [%sexp (Float.pi : float)];
+  print_s [%sexp (1e2 |> model_pi |> model_to_pi : int)];
+  print_s [%sexp (1e4 |> model_pi |> model_to_pi : int)];
+  print_s [%sexp (1e6 |> model_pi |> model_to_pi : int)];
+  print_s [%sexp (1e8 |> model_pi |> model_to_pi : int)];
   [%expect {|
+    3.1415926535897931
     31
     314
     3141
