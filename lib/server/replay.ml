fba208a9fc19e51f8e9d01f8b64b047353c307a4
Vladislav Prekel <misterptits@yandex.ru>
2022-05-12 21:46:57 +0700

    fix

--- lib/server/replay.ml
+++ lib/server/replay.ml
@@ -1,11 +1,10 @@
-open Core
+(* open Core
 open Lwt.Let_syntax
 
 module Make
     (C : Engine.Module_types.CONSTS)
     (S : module type of Engine.Scene.Make (C)) =
     struct
-  module Store = Irmin_unix.Git.FS.KV (Irmin.Contents.String)
 
   let repo_field = Dream.new_field ()
   let repo_var = ref None
@@ -18,11 +17,7 @@ module Make
     | None -> Dream.empty `Internal_Server_Error
   ;;
 
-  let init_store dir =
-    let%map repo = Store.Repo.v (Irmin_git.config ~bare:true dir) in
-    repo_var := Some repo;
-    Lwt.return_unit
-  ;;
+
 
   let route =
     Dream.scope
@@ -31,11 +26,11 @@ module Make
       [ Dream.get "/:replay_id" (fun request ->
             match Dream.field request repo_field with
             | Some repo ->
-              let replay_id = Dream.param request "replay_id" in
+              (* let replay_id = Dream.param request "replay_id" in
               let%bind t = Store.of_branch repo replay_id in
-              let%bind _m = Store.get t [ "replay.scm" ] in
+              let%bind _m = Store.get t [ "replay.scm" ] in *)
               assert false
             | None -> Dream.empty `Internal_Server_Error)
       ]
   ;;
-end
+end *)
