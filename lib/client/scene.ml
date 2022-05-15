5e79b289fed0329ab79d3de6064039a15a3de68b
Vladislav Prekel <misterptits@yandex.ru>
2022-05-15 12:28:37 +0700

    move

--- lib/client/scene.ml
+++ lib/client/scene.ml
@@ -7,6 +7,37 @@ module Svg = Virtual_dom_svg
 let svg_ref = ref None
 let pt_ref = ref None
 
+let svg_click_coords (evt : Dom_html.mouseEvent Js.t) =
+  match
+    evt##.target
+    |> Js.Opt.to_option
+    |> Option.map ~f:Dom_svg.CoerceTo.element
+    |> Option.bind ~f:Js.Opt.to_option
+  with
+  | Some e ->
+    let svg =
+      match e##.ownerSVGElement |> Js.some |> Js.Opt.to_option with
+      | Some e -> e
+      | None -> Dom_svg.CoerceTo.svg e |> Js.Opt.to_option |> Option.value_exn
+    in
+    let pt =
+      match !svg_ref, !pt_ref with
+      | Some svg, Some pt when phys_equal svg e##.ownerSVGElement -> pt
+      | _ ->
+        let pt = svg##createSVGPoint in
+        svg_ref := Some e##.ownerSVGElement;
+        pt_ref := Some pt;
+        pt
+    in
+    (Obj.magic pt)##.x := evt##.clientX;
+    (Obj.magic pt)##.y := evt##.clientY;
+    let cursorpt = pt##matrixTransform svg##getScreenCTM##inverse in
+    let x = cursorpt##.x in
+    let y = cursorpt##.y in
+    x, y
+  | None -> assert false
+;;
+
 module Make
     (C : Engine.Module_types.CONSTS with module N = Float)
     (S : module type of Engine.Scene.Make (C)) =
@@ -16,34 +47,10 @@ module Make
     let y = S.Values.get_scalar_exn figure.values ~var:`y0 in
     let r = S.Values.get_scalar_exn figure.values ~var:`r in
     let on_click id (evt : Dom_html.mouseEvent Js.t) =
-      let t1 = evt##.target |> Js.Opt.to_option in
-      let t2 =
-        t1
-        |> Option.map ~f:Dom_svg.CoerceTo.element
-        |> Option.bind ~f:Js.Opt.to_option
-        |> Option.map ~f:Dom_svg.CoerceTo.circle
-        |> Option.bind ~f:Js.Opt.to_option
-      in
-      match t1, t2 with
-      | Some _, Some e ->
-        let svg = e##.ownerSVGElement in
-        let pt =
-          match !svg_ref, !pt_ref with
-          | Some svg, Some pt when phys_equal svg e##.ownerSVGElement -> pt
-          | _ ->
-            let pt = svg##createSVGPoint in
-            svg_ref := Some e##.ownerSVGElement;
-            pt_ref := Some pt;
-            pt
-        in
-        (Obj.magic pt)##.x := evt##.clientX;
-        (Obj.magic pt)##.y := evt##.clientY;
-        let cursorpt = pt##matrixTransform svg##getScreenCTM##inverse in
-        let x = cursorpt##.x -. x in
-        let y = cursorpt##.y -. y in
-        printf "x: %f, y: %f\n" x y;
-        body_click id x y r
-      | _ -> assert false
+      let sx, sy = svg_click_coords evt in
+      let nx = sx -. x in
+      let ny = sy -. y in
+      body_click id nx ny r
     in
     let open Vdom in
     Svg.Node.circle
@@ -86,11 +93,22 @@ module Make
       ~(body_click : (S.Body.Id.t -> float -> float -> float -> unit Ui_effect.t) Value.t)
       ~time
       ~viewbox
+      ~move_viewbox
     =
+    let%sub mouse_down_coords, set_mouse_down_coords =
+      Bonsai.state_opt
+        [%here]
+        (module struct
+          type t = float * float [@@deriving sexp, equal]
+        end)
+    in
     let%arr (scene : S.Scene.t) = scene
     and body_click = body_click
     and time = time
-    and min_x, min_y, width, height = viewbox in
+    and min_x, min_y, width, height = viewbox
+    and mouse_down_coords = mouse_down_coords
+    and set_mouse_down_coords = set_mouse_down_coords
+    and move_viewbox = move_viewbox in
     let t = time -. scene.time in
     let circles =
       scene.bodies
@@ -107,15 +125,25 @@ module Make
     Svg.Node.svg
       ~attr:
         (Attr.many
-           [ Attr.on_mousewheel (fun _event ->
-                 (* Js_of_ocaml.Firebug.console##log event; *)
+           [ Attr.on_mousewheel (fun event ->
+                 Js_of_ocaml.Firebug.console##log event;
                  Effect.Ignore)
-           ; Attr.on_mousedown (fun _event ->
+           ; Attr.on_mousedown (fun event ->
+                 let ox, oy = svg_click_coords event in
+                 printf "ox %f oy %f\n" ox oy;
                  (* Js_of_ocaml.Firebug.console##log event; *)
-                 Effect.Ignore)
-           ; Attr.on_mouseup (fun _event ->
-                 (* Js_of_ocaml.Firebug.console##log event; *)
-                 Effect.Ignore)
+                 set_mouse_down_coords (Some (ox, oy)))
+           ; Attr.on_mouseup (fun event ->
+                 let nx, ny = svg_click_coords event in
+                 printf "nx %f ny %f\n" nx ny;
+                 let%bind.Effect () =
+                   match mouse_down_coords with
+                   | Some (ox, oy) ->
+                     printf "dx %f dy %f\n" (nx -. ox) (ny -. oy);
+                     move_viewbox (nx -. ox, ny -. oy)
+                   | None -> Effect.Ignore
+                 in
+                 set_mouse_down_coords None)
            ; Svg.Attr.viewbox ~min_x ~min_y ~width ~height
            ])
       all
@@ -125,13 +153,13 @@ module Make
   let to_prec a = a *. 1000.
   let of_prec a = a /. 1000.
 
-  let scene1 ~model ~time ~body_click ~viewbox =
+  let scene1 ~model ~time ~body_click ~viewbox ~move_viewbox =
     let%sub scene =
       let%arr model = model
       and time = time in
       model.S.Model.scenes |> S.Scenes.before ~time |> snd
     in
-    scene_frame ~scene ~time ~body_click ~viewbox
+    scene_frame ~scene ~time ~body_click ~viewbox ~move_viewbox
   ;;
 
   let calc_new_time_every ~model ~time ~update_time ~is_pause ~speed =
@@ -158,6 +186,138 @@ module Make
     Bonsai.Clock.every [%here] (Time_ns.Span.of_sec frame_time60) calc_new_time
   ;;
 
+  let time_panel ~time ~set_time =
+    let%arr time = time
+    and set_time = set_time in
+    let open Vdom in
+    let open Node in
+    div
+      [ input
+          ~attr:
+            (Attr.many
+               [ Attr.type_ "range"
+               ; Attr.min (to_prec 0.)
+               ; Attr.max (to_prec 100.)
+               ; Attr.on_change (fun _ a ->
+                     try a |> Float.of_string |> of_prec |> set_time with
+                     | _ -> Effect.Ignore)
+               ; Attr.value_prop (Float.to_string (to_prec time))
+               ])
+          []
+      ; input
+          ~attr:
+            (Attr.many
+               [ Attr.on_change (fun _ a ->
+                     try a |> Float.of_string |> set_time with
+                     | _ -> Effect.Ignore)
+               ; Attr.value (Float.to_string time)
+               ])
+          []
+      ]
+  ;;
+
+  let speed_panel ~speed ~set_speed =
+    let%arr speed = speed
+    and set_speed = set_speed in
+    let open Vdom in
+    let open Node in
+    div
+      [ input
+          ~attr:
+            (Attr.many
+               [ Attr.type_ "range"
+               ; Attr.min (to_prec (-25.))
+               ; Attr.max (to_prec 25.)
+               ; Attr.on_input (fun _ a ->
+                     try a |> Float.of_string |> of_prec |> set_speed with
+                     | _ -> Effect.Ignore)
+               ; Attr.value (Float.to_string speed)
+               ])
+          []
+      ; input
+          ~attr:
+            (Attr.many
+               [ Attr.on_change (fun _ a ->
+                     try a |> Float.of_string |> set_speed with
+                     | _ -> Effect.Ignore)
+               ; Attr.value (Float.to_string speed)
+               ])
+          []
+      ]
+  ;;
+
+  let timeoutd_panel ~timeoutd ~set_timeoutd =
+    let%arr timeoutd = timeoutd
+    and set_timeoutd = set_timeoutd in
+    let open Vdom in
+    let open Node in
+    div
+      [ input
+          ~attr:
+            (Attr.many
+               [ Attr.type_ "range"
+               ; Attr.min (to_prec 0.)
+               ; Attr.max (to_prec 100.)
+               ; Attr.on_input (fun _ a ->
+                     try a |> Float.of_string |> of_prec |> set_timeoutd with
+                     | _ -> Effect.Ignore)
+               ; Attr.value (Float.to_string (to_prec timeoutd))
+               ])
+          []
+      ; input
+          ~attr:
+            (Attr.many
+               [ Attr.on_change (fun _ a ->
+                     try a |> Float.of_string |> set_timeoutd with
+                     | _ -> Effect.Ignore)
+               ; Attr.value (Float.to_string timeoutd)
+               ])
+          []
+      ]
+  ;;
+
+  let viewbox_panel ~viewbox_init =
+    let%sub viewbox, set_viewbox =
+      Bonsai_extra.state_dynamic_model
+        [%here]
+        (module struct
+          type t = float * float * float * float [@@deriving sexp, equal]
+        end)
+        ~model:(`Given viewbox_init)
+    in
+    let open Vdom in
+    let open Node in
+    let%arr ((min_x, min_y, width, height) as viewbox) = viewbox
+    and set_viewbox = set_viewbox in
+    let set_min_x min_x = set_viewbox (min_x, min_y, width, height) in
+    let set_min_y min_y = set_viewbox (min_x, min_y, width, height) in
+    let set_width width = set_viewbox (min_x, min_y, width, height) in
+    let set_height height = set_viewbox (min_x, min_y, width, height) in
+    let inp a set =
+      input
+        ~attr:
+          (Attr.many
+             [ Attr.on_change (fun _ x ->
+                   try x |> Float.of_string |> set with
+                   | _ -> Effect.Ignore)
+             ; Attr.value (Float.to_string a)
+             ])
+        []
+    in
+    ( viewbox
+    , (fun (dx, dy) -> set_viewbox (min_x -. dx, min_y -. dy, width -. dx, height -. dy))
+    , div
+        [ inp min_x set_min_x
+        ; br ()
+        ; inp min_y set_min_y
+        ; br ()
+        ; inp width set_width
+        ; br ()
+        ; inp height set_height
+        ; br ()
+        ] )
+  ;;
+
   let scene ~(state : S.Model.t Value.t) ~dispatch =
     let%sub time, update_time = Bonsai.state [%here] (module Float) ~default_model:0. in
     let%sub is_pause, set_is_pause =
@@ -171,41 +331,40 @@ module Make
     let%sub text_state, set_text_state =
       Bonsai.state [%here] (module String) ~default_model:""
     in
-    let cl =
-      dispatch
-      >>| (fun a timeoutd time id x y r ->
-            a
-              (`Action
-                S.Action.
-                  { time
-                  ; action =
-                      S.Action.GiveVelocity
-                        { id; v0 = Float.(x / r * -200., y / r * -200.) }
-                  ; timeout = Some Float.(time + timeoutd)
-                  }))
-      <*> timeoutd
-      <*> time
+    let%sub body_click =
+      let%arr time = time
+      and timeoutd = timeoutd
+      and dispatch = dispatch in
+      fun id x y r ->
+        dispatch
+          (`Action
+            S.Action.
+              { time
+              ; action =
+                  S.Action.GiveVelocity { id; v0 = Float.(x / r * -200., y / r * -200.) }
+              ; timeout = Some Float.(time + timeoutd)
+              })
     in
-    let%sub frame =
-      scene1
-        ~model:state
-        ~time
-        ~body_click:cl
-        ~viewbox:(Bonsai.Value.return (0., 0., 1000., 1000.))
+    let%sub viewbox, move_viewbox, viewbox_panel =
+      viewbox_panel ~viewbox_init:(Bonsai.Value.return (0., 0., 1000., 1000.))
     in
+    let%sub frame = scene1 ~model:state ~time ~body_click ~viewbox ~move_viewbox in
+    let%sub speed_panel = speed_panel ~speed ~set_speed in
+    let%sub time_panel = time_panel ~time ~set_time:update_time in
+    let%sub timeoutd_panel = timeoutd_panel ~timeoutd ~set_timeoutd in
     let%arr time = time
     and frame = frame
     and dispatch = dispatch
     and is_pause = is_pause
     and set_is_pause = set_is_pause
-    and speed = speed
-    and set_speed = set_speed
-    and set_time = update_time
+    and time_panel = time_panel
+    and timeoutd_panel = timeoutd_panel
     and text_state = text_state
     and set_text_state = set_text_state
     and state = state
     and timeoutd = timeoutd
-    and set_timeoutd = set_timeoutd in
+    and speed_panel = speed_panel
+    and viewbox_panel = viewbox_panel in
     Vdom.(
       ( Node.div
           [ Node.text (Float.to_string time)
@@ -268,71 +427,13 @@ module Make
               ~attr:(Attr.on_click (fun _ -> set_is_pause (not is_pause)))
               [ Node.text (if is_pause then "unpause" else "pause") ]
           ; Node.br ()
-          ; Node.input
-              ~attr:
-                (Attr.many
-                   [ Attr.type_ "range"
-                   ; Attr.min (to_prec (-25.))
-                   ; Attr.max (to_prec 25.)
-                   ; Attr.on_input (fun _ a ->
-                         try a |> Float.of_string |> of_prec |> set_speed with
-                         | _ -> Effect.Ignore)
-                   ; Attr.value (Float.to_string speed)
-                   ])
-              []
-          ; Node.input
-              ~attr:
-                (Attr.many
-                   [ Attr.on_change (fun _ a ->
-                         try a |> Float.of_string |> set_speed with
-                         | _ -> Effect.Ignore)
-                   ; Attr.value (Float.to_string speed)
-                   ])
-              []
+          ; speed_panel
           ; Node.br ()
-          ; Node.input
-              ~attr:
-                (Attr.many
-                   [ Attr.type_ "range"
-                   ; Attr.min (to_prec 0.)
-                   ; Attr.max (to_prec 100.)
-                   ; Attr.on_change (fun _ a ->
-                         try a |> Float.of_string |> of_prec |> set_time with
-                         | _ -> Effect.Ignore)
-                   ; Attr.value_prop (Float.to_string (to_prec time))
-                   ])
-              []
-          ; Node.input
-              ~attr:
-                (Attr.many
-                   [ Attr.on_change (fun _ a ->
-                         try a |> Float.of_string |> set_time with
-                         | _ -> Effect.Ignore)
-                   ; Attr.value (Float.to_string time)
-                   ])
-              []
+          ; time_panel
           ; Node.br ()
-          ; Node.input
-              ~attr:
-                (Attr.many
-                   [ Attr.type_ "range"
-                   ; Attr.min (to_prec 0.)
-                   ; Attr.max (to_prec 100.)
-                   ; Attr.on_input (fun _ a ->
-                         try a |> Float.of_string |> of_prec |> set_timeoutd with
-                         | _ -> Effect.Ignore)
-                   ; Attr.value (Float.to_string (to_prec timeoutd))
-                   ])
-              []
-          ; Node.input
-              ~attr:
-                (Attr.many
-                   [ Attr.on_change (fun _ a ->
-                         try a |> Float.of_string |> set_timeoutd with
-                         | _ -> Effect.Ignore)
-                   ; Attr.value (Float.to_string timeoutd)
-                   ])
-              []
+          ; timeoutd_panel
+          ; Node.br ()
+          ; viewbox_panel
           ; Node.br ()
           ; Node.button
               ~attr:
@@ -350,9 +451,6 @@ module Make
                      `Action { time; action = S.Action.Empty; timeout = None } |> dispatch))
               [ Node.text "no timeout" ]
           ; Node.br ()
-          ]
-      , Node.div
-          [ frame
           ; Node.textarea
               ~attr:
                 (Attr.many
@@ -367,6 +465,7 @@ module Make
                          |> set_text_state)
                    ])
               [ Node.text "to text" ]
-          ] ))
+          ]
+      , Node.div [ frame ] ))
   ;;
 end
