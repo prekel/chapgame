ae139a797cd16d8156b79147165762e3b882ff38
Vladislav Prekel <misterptits@yandex.ru>
2022-06-09 11:18:50 +0700

    started 2 tables

---
+++ lib/engine/action.mli
@@ -0,0 +1,38 @@
+type a =
+  | AddBody of
+      { id : Body.Id.t option
+      ; x0 : float
+      ; y0 : float
+      ; r : float
+      ; mu : float
+      ; m : float
+      }
+  | AddBodyOfValues of (Body.Id.t option * (Open.Var.t * float) list)
+  | AddPoint of Point.t
+  | AddLine of Line.t
+  | AddLineWithPoints of Line.t
+  | GiveVelocity of
+      { id : Body.Id.t
+      ; v0 : float * float
+      }
+  | RemoveBody of Body.Id.t
+  | RemoveLine of Line.t
+  | RemovePoint of Point.t
+  | UpdateBody of (Body.Id.t * (Open.Var.t * float) list)
+  | UpdateLine of Line.t * Line.t
+  | UpdatePoint of Point.t * Point.t
+  | UpdateGlobal of (Open.Var.t * float)
+[@@deriving sexp, equal, compare]
+
+type until =
+  { timespan : float option
+  ; quantity : int option
+  }
+[@@deriving sexp, equal, compare]
+
+type t =
+  { time : float
+  ; action : a
+  ; until : until
+  }
+[@@deriving sexp, equal, compare]
